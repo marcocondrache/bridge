6747c1a06480438193fbf9f6fb48d35d7eb5da60
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-11-11 22:22:47 +0100

    feat: tab setup
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- crates/ui/src/components.rs
+++ crates/ui/src/components.rs
@@ -2,4 +2,5 @@ pub mod button;
 pub mod input;
 pub mod label;
 pub mod resize_handle;
+pub mod tab;
 pub mod table;
---
+++ crates/ui/src/components/tab.rs
@@ -0,0 +1,41 @@
+use gpui::{
+    AnyElement, Div, ElementId, InteractiveElement, ParentElement, RenderOnce, Stateful,
+    StatefulInteractiveElement, div,
+};
+use smallvec::SmallVec;
+
+pub struct Tab {
+    base: Stateful<Div>,
+    selected: bool,
+    children: SmallVec<[AnyElement; 2]>,
+}
+
+impl Tab {
+    pub fn new(id: impl Into<ElementId>) -> Self {
+        Self {
+            base: div().id(id.into()),
+            selected: false,
+            children: SmallVec::new(),
+        }
+    }
+}
+
+impl InteractiveElement for Tab {
+    fn interactivity(&mut self) -> &mut gpui::Interactivity {
+        self.base.interactivity()
+    }
+}
+
+impl StatefulInteractiveElement for Tab {}
+
+impl ParentElement for Tab {
+    fn extend(&mut self, elements: impl IntoIterator<Item = AnyElement>) {
+        self.children.extend(elements)
+    }
+}
+
+impl RenderOnce for Tab {
+    fn render(self, window: &mut gpui::Window, cx: &mut gpui::App) -> impl gpui::IntoElement {
+        self.base
+    }
+}
